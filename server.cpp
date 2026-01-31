c791bc2d95c1ab5e5b7ae1f2899e3229b3ca7011
Andezion <245122@edu.p.lodz.pl>
2026-01-31 23:57:15 +0100

    added new server

--- server.cpp
+++ server.cpp
@@ -136,9 +136,7 @@ private:
             [this](const boost::system::error_code &ec, tcp::socket socket) {
                 if (!ec)
                 {
-                    std::cout << "New connection from: "
-                              << socket.remote_endpoint().address().to_string()
-                              << std::endl;
+                    std::cout << "New connection accepted" << std::endl;
                     std::make_shared<ChatSession>(std::move(socket), room_)->start();
                 }
                 accept();
