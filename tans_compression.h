2025a51fda4ea6876ac6dcde73f3ff9c68b8148a
Andezion <245122@edu.p.lodz.pl>
2026-02-04 17:10:28 +0100

    add: tans integration

--- tans_compression.h
+++ tans_compression.h
@@ -3,6 +3,5 @@
 #include <vector>
 #include <cstddef>
 
-// tANS compression for short messages
 std::vector<unsigned char> tans_encode(const unsigned char* data, size_t len);
 std::vector<unsigned char> tans_decode(const unsigned char* data, size_t len);
